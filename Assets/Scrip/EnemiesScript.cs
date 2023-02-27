dca4be5ccd011b05f61c4f97b89b93fdce34f335
hunganhcao <hunganh03102002@gmail.com>
2023-02-27 23:23:34 +0700

    new BT2

--- Assets/Scrip/EnemiesScript.cs
+++ Assets/Scrip/EnemiesScript.cs
@@ -20,6 +20,9 @@ public class EnemiesScript : MonoBehaviour
     private Text txtLife;
 	public GameObject DiedPnUI;
     private bool clickable=false;
+    private bool fadeOut=false;
+    private float fadeSpeed = 2;
+    private bool moveable=true;
 
 	private void Start()
     {
@@ -27,8 +30,8 @@ public class EnemiesScript : MonoBehaviour
         b2d= GetComponent<BoxCollider2D>();
         txtScore = GameObject.Find("ScoreTxt").GetComponent<Text>();
         txtLife = GameObject.Find("LifeTxt").GetComponent<Text>();
-        
 
+        moveable = true;
 
     }
     void Update()
@@ -44,21 +47,40 @@ public class EnemiesScript : MonoBehaviour
                 clickable= true;
             }
         }
-        
+        if (fadeOut)
+
+        {
+            Color objectColor = this.GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.GetComponent<Renderer>().material.color = objectColor;
+
+            if (objectColor.a <= 0)
+            {
+                fadeOut = false; 
+                Destroy(this.gameObject); 
+            }
+               
+        }
+
     }
     private void FixedUpdate()
     {
-        System.Random rnd = new System.Random();
-        for (int i = 0; i < 10; i++)
+        if (moveable == true)
         {
-            var temp = rnd.Next(4, 15);
-            VanTocVat = temp;
+            System.Random rnd = new System.Random();
+            for (int i = 0; i < 10; i++)
+            {
+                var temp = rnd.Next(4, 15);
+                VanTocVat = temp;
+            }
+            
+            Vector2 Dichuyen = transform.position;
+            if (DiChuyenTrai) { Dichuyen.x -= VanTocVat * Time.deltaTime; }
+            else Dichuyen.x += VanTocVat * Time.deltaTime;
+            transform.position = Dichuyen;
         }
-        Vector2 Dichuyen =transform.position;
-        if (DiChuyenTrai) { Dichuyen.x -= VanTocVat * Time.deltaTime; }
-        else Dichuyen.x += VanTocVat * Time.deltaTime;
-        transform.position = Dichuyen;
-        
     }
     private void OnMouseDown()
     {
@@ -66,9 +88,11 @@ public class EnemiesScript : MonoBehaviour
         {
             int score = Int32.Parse(txtScore.text);
             score += 2;
-
             txtScore.text = "" + score.ToString();
-            Destroy(this.gameObject);
+            moveable=false;
+            FadeOutObject(); fadeOut = true;
+             new WaitForSeconds(1.5f);
+            
         }
 
     }
@@ -124,6 +148,10 @@ public class EnemiesScript : MonoBehaviour
         Vector2 HuongQuay = transform.localScale;
         HuongQuay.x *= (-1);
         transform.localScale = HuongQuay;
+    }
+    private void FadeOutObject()
+    {
+        fadeOut = true;
     }
 
 }
